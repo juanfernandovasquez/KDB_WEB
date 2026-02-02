68258825781fcb0846c4b2345836b151f9cdd8b8
juanfernandovasquez <juanvasquezvergara@gmail.com>
2026-02-01 20:13:51 -0500

    cache-bust admin modules

--- frontend/admin/main.js
+++ frontend/admin/main.js
@@ -1,8 +1,8 @@
 // Entry point for the admin panel (ES module).
 // We keep loading the legacy admin.js (which wires up all sections)
 // while progressively extracting shared pieces into modules.
-import "./utils.js";
-import "./editors.js";
-import "./admin.js";
+import "./utils.js?v=20260202";
+import "./editors.js?v=20260202";
+import "./admin.js?v=20260202";
 
 console.info("admin main module loaded (legacy bootstrap + modules)");
